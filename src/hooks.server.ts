3b902e4d0001c734bfc2f65c017a8e20f1895814
adibkadir <adib.kadir@gmail.com>
2023-05-08 01:01:20 -0700

    Update hooks.server.ts
    SignIn checks still allow access even though '/unauthorized' is returned.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,28 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
-import GitHub from "@auth/core/providers/github"
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
+import GoogleProvider from "@auth/core/providers/google"
+import { GOOGLE_ID, GOOGLE_SECRET } from "$env/static/private"
 
 export const handle = SvelteKitAuth({
-  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+  providers: [
+    GoogleProvider({
+      clientId: GOOGLE_ID,
+      clientSecret: GOOGLE_SECRET,
+      authorization: { params: { prompt: 'consent' } },
+    }),
+  ],
+
+
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const isAllowedToSignIn = false
+      if (isAllowedToSignIn) {
+        return true
+      } else {
+        // Return false to display a default error message
+        // return false
+        // Or you can return a URL to redirect to:
+        return '/unauthorized'
+      }
+    }
+  }
 })
